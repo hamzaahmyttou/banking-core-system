205e1a89cb712a2a34eaa0de6998d6a57e45c29e
Hamza Ahmyttou <hamza.ahmyttou@outlook.com>
2026-06-24 13:47:20 +0100

    first commit

---
+++ src/main/java/com/banking_core_system/banking_core_system/BankingCoreSystemApplication.java
@@ -0,0 +1,13 @@
+package com.banking_core_system.banking_core_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingCoreSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingCoreSystemApplication.class, args);
+	}
+
+}
