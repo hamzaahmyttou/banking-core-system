36e6811e399d83e5621b5761bf5ee5a5973ac01e
Hamza Ahmyttou <hamza.ahmyttou@outlook.com>
2026-07-15 20:05:07 +0100

    TransactionController ResponseEntity

--- src/main/java/com/banking_core_system/banking_core_system/transaction/controller/TransactionController.java
+++ src/main/java/com/banking_core_system/banking_core_system/transaction/controller/TransactionController.java
@@ -5,6 +5,7 @@ import com.banking_core_system.banking_core_system.transaction.service.Transacti
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,40 +18,50 @@ public class TransactionController {
     private final TransactionService transactionService;
 
     @PostMapping("/deposit")
-    @ResponseStatus(HttpStatus.CREATED)
-    public TransactionResponse deposit(
+    public ResponseEntity<TransactionResponse> deposit(
             @Valid @RequestBody DepositRequest request) {
 
-        return transactionService.deposit(request);
+        return new ResponseEntity<>(
+                transactionService.deposit(request),
+                HttpStatus.CREATED
+        );
     }
 
     @PostMapping("/withdraw")
-    @ResponseStatus(HttpStatus.CREATED)
-    public TransactionResponse withdraw(
+    public ResponseEntity<TransactionResponse> withdraw(
             @Valid @RequestBody WithdrawalRequest request) {
 
-        return transactionService.withdraw(request);
+        return new ResponseEntity<>(
+                transactionService.withdraw(request),
+                HttpStatus.CREATED
+        );
     }
 
     @PostMapping("/transfer")
-    @ResponseStatus(HttpStatus.CREATED)
-    public TransferResponse transfer(
+    public ResponseEntity<TransferResponse> transfer(
             @Valid @RequestBody TransferRequest request) {
 
-        return transactionService.transfer(request);
+        return new ResponseEntity<>(
+                transactionService.transfer(request),
+                HttpStatus.CREATED
+        );
     }
 
     @GetMapping("/{id}")
-    public TransactionResponse getTransactionById(
+    public ResponseEntity<TransactionResponse> getTransactionById(
             @PathVariable Long id) {
 
-        return transactionService.getTransactionById(id);
+        return ResponseEntity.ok(
+                transactionService.getTransactionById(id)
+        );
     }
 
     @GetMapping("/account/{accountId}")
-    public List<TransactionResponse> getAccountHistory(
+    public ResponseEntity<List<TransactionResponse>> getAccountHistory(
             @PathVariable Long accountId) {
 
-        return transactionService.getAccountHistory(accountId);
+        return ResponseEntity.ok(
+                transactionService.getAccountHistory(accountId)
+        );
     }
 }
